7b1ab793f1336870f0f17beb3a6ce4dff96cfc4c
Roy <elkabetzroy@gmail.com>
2019-05-27 14:48:49 +0300

    simple update
    do:
    - implement the ITE in an even-odd order.
    - check why ITE diverge
    - implement ITE gauge fixing

--- simple_update_algorithm.py
+++ simple_update_algorithm.py
@@ -1,4 +1,6 @@
+from scipy import linalg
 import numpy as np
+import Tensor_Network_contraction as tnc
 import copy as cp
 
 
@@ -13,7 +15,7 @@ def simple_update(TT, LL, Uij, imat, smat, D_max):
     """
     :param TT: list of tensors in the tensor network TT = [T1, T2, T3, ..., Tp]
     :param LL: list of lists of the lambdas LL = [L1, L2, ..., Ls]
-    :param Uij: the interaction matrix which assumed to be the same for all interactions
+    :param hij: the interaction matrix which assumed to be the same for all interactions
     :param imat: The index matrix which indicates which tensor connect to which edge (as indicated in the papper)
     :param smat: The structure matrix which indicates which leg of every tensor is connected to which edge
     :param D_max: maximal virtual dimension
@@ -29,6 +31,7 @@ def simple_update(TT, LL, Uij, imat, smat, D_max):
         Ti = [TT[tidx[0]], tdim[0]]
         Tj = [TT[tidx[1]], tdim[1]]
         lamda_k = LL[Ek]
+        #print_shape(lamda_k, 'lamda_k')
 
         # collecting all neighboring (edges, legs)
         Ti_alldim = [list(np.nonzero(imat[tidx[0], :])[0]), list(smat[tidx[0], np.nonzero(imat[tidx[0], :])[0]])]
@@ -58,54 +61,99 @@ def simple_update(TT, LL, Uij, imat, smat, D_max):
         j_perm[[1, Tj[1]]] = j_perm[[Tj[1], 1]]
 
         # reshaping Ti, Tj
+        #print('\n')
+        #print('reshaping Ti, Tj')
         i_prod = np.delete(np.array(Ti[0].shape), [0, Ti[1]])
         j_prod = np.delete(np.array(Tj[0].shape), [0, Tj[1]])
         i_shape = [Ti[0].shape[0], Ti[0].shape[Ti[1]], np.prod(i_prod)]
         j_shape = [Tj[0].shape[0], Tj[0].shape[Tj[1]], np.prod(j_prod)]
-        print('Ti, Tj, shapes: ', Ti[0].shape, Tj[0].shape)
         Pl, i_revperm, i_revshape = permshape(Ti[0], i_perm, i_shape)
         Pr, j_revperm, j_revshape = permshape(Tj[0], j_perm, j_shape)
-        print('Pl, Pr, shapes: ', Pl.shape, Pr.shape)
+        #print_shape(Ti[0], 'Ti[0]')
+        #print_shape(Tj[0], 'Tj[0]')
+        #print_shape(Pl, 'Pl')
+        #print_shape(Pr, 'Pr')
 
 
         ## (d) QR/LQ decompose Pl, Pr to obtain Q1, R and L, Q2 sub-tensors, respectively
+        #print('\n')
+        #print('(d) QR/LQ decompose Pl, Pr to obtain Q1, R and L, Q2 sub-tensors, respectively')
         Pl = np.transpose(np.reshape(Pl, (i_shape[0] * i_shape[1], i_shape[2])))
         Pr = np.transpose(np.reshape(Pr, (j_shape[0] * j_shape[1], j_shape[2])))
-        print('Pl, Pr, shapes: ', Pl.shape, Pr.shape)
         Q1, R = np.linalg.qr(Pl, 'complete')
         Q2, L = np.linalg.qr(Pr, 'complete')
-        print('Q1, R shapes: ', Q1.shape, R.shape)
-        print('Q2, L shapes: ', Q2.shape, L.shape)
         R = np.reshape(R, (R.shape[0], i_shape[0], R.shape[1] / i_shape[0]))
         L = np.reshape(L, (L.shape[0], j_shape[0], L.shape[1] / j_shape[0]))
-        print('R, L shapes: ', R.shape, L.shape)
+        #print_shape(Pl, 'Pl')
+        #print_shape(Pr, 'Pr')
+        #print_shape(Q1, 'Q1')
+        #print_shape(R, 'R')
+        #print_shape(Q2, 'Q2')
+        #print_shape(L, 'L')
 
         ## (e) Contract the ITE gate Ui,Ek , with R, L and lambda_k to form theta tensor.
+        #print('\n')
+        #print('(e) Contract the ITE gate Ui,Ek , with R, L and lambda_k to form theta tensor.')
         theta = imaginary_time_evolution(R, L, lamda_k, Uij)
+        #print_shape(theta, 'theta')
 
         # reshaping theta into a matrix
+        #print('\n')
+        #print('reshaping theta into a matrix')
+        new_shape1 = [theta.shape[0] * theta.shape[1], theta.shape[2], theta.shape[3]]
+        new_shape2 = [theta.shape[0] * theta.shape[1], theta.shape[2] * theta.shape[3]]
+        theta = np.reshape(theta, new_shape1)
+        theta = np.reshape(theta, new_shape2)
+        print('Ek = ', Ek)
+        print('theta sum = ', np.sum(theta))
+        #print_shape(theta, 'theta')
 
         ## (f) Obtain R', L', lambda'_k tensors by applying an SVD to theta and truncating
         ##     the tensors by keeping the D largest singular values.
-        Rtild, lamda_ktild, Ltild = np.linalg.svd(theta, full_matrices=True)
+        #print('\n')
+        #print('theta SVD')
+        Rtild, lamda_ktild, Ltild = np.linalg.svd(theta)
         Ltild = np.transpose(Ltild)
-        # trancate
-        lamda_ktild = lamda_ktild[0:D_max - 1] if D_max < len(lamda_ktild) else lamda_ktild
-        Rtild = Rtild[:, 0:D_max - 1] if D_max < len(lamda_ktild) else Rtild
-        Ltild = Ltild[:, 0:D_max - 1] if D_max < len(lamda_ktild) else Ltild
+        #print_shape(Rtild, 'Rtild')
+        #print_shape(Ltild, 'Ltild')
+        #print_shape(lamda_ktild, 'lamda_ktild')
+        #print('\n')
+        #print('trancation')
+        # trancating the SVD results up to D_max eigenvalues
+        Rtild = Rtild[:, 0:D_max] if D_max < len(lamda_ktild) else Rtild
+        Ltild = Ltild[:, 0:D_max] if D_max < len(lamda_ktild) else Ltild
+        lamda_ktild = lamda_ktild[0:D_max] if D_max < len(lamda_ktild) else lamda_ktild
+        #print_shape(Rtild, 'Rtild')
+        #print_shape(Ltild, 'Ltild')
+        #print_shape(lamda_ktild, 'lamda_ktild')
 
         # reshaping R' and L' to rank(3) tensors
+        i_revshape[1] = Rtild.shape[1]
+        j_revshape[1] = Ltild.shape[1]
         Rtild = np.reshape(Rtild, (Rtild.shape[0] / i_shape[0], i_shape[0], Rtild.shape[1]))
         Ltild = np.reshape(Ltild, (Ltild.shape[0] / j_shape[0], j_shape[0], Ltild.shape[1]))
+        #print('\n')
+        #print('reshaping')
+        #print_shape(Rtild, 'Rtild')
+        #print_shape(Ltild, 'Ltild')
+        #print_shape(lamda_ktild, 'lamda_ktild')
 
         ## (g) Glue back the R', L', sub-tensors to Q1, Q2, respectively, to form updated tensors P'l, P'r.
         # also reshaping both into (d_spin, D_max, d_virtual^3)
         Pltild = np.einsum('ij,jkl->kli', Q1, Rtild)
         Prtild = np.einsum('ij,jkl->kli', Q2, Ltild)
+        #print('\n')
+        #print('gluing')
+        #print_shape(Pltild, 'Pltild')
+        #print_shape(Prtild, 'Prtild')
 
         ## (h) Reshape back the P`l, P`r to the original rank-(z + 1) tensors T'_i, T'_j
-        Titild = permshape(Pltild, i_revperm, i_revshape, 'reverse')
-        Tjtild = permshape(Prtild, j_revperm, j_revshape, 'reverse')
+        #print('\n')
+        #print('(h) Reshape back the P`l, P`r to the original rank-(z + 1) tensors T_i, T_j')
+        Titild, _, _ = permshape(Pltild, i_revperm, i_revshape, 'reverse')
+        Tjtild, _, _ = permshape(Prtild, j_revperm, j_revshape, 'reverse')
+        #print_shape(Titild, 'Titild')
+        #print_shape(Tjtild, 'Tjtild')
 
         ## (i) Remove bond matrices lambda_m from virtual legs m != Ek to obtain the updated tensors Ti~, Tj~.
         for Em in range(len(Ti_lamdas)):
@@ -113,6 +161,14 @@ def simple_update(TT, LL, Uij, imat, smat, D_max):
                               range(len(Titild.shape)))
             Tjtild = np.einsum(Tjtild, range(len(Tjtild.shape)), Tj_lamdas[Em] ** (-1), [Tj_alldim[1][Em]],
                               range(len(Tjtild.shape)))
+        TT[tidx[0]] = cp.copy(Titild)
+        TT[tidx[1]] = cp.copy(Tjtild)
+        LL[Ek] = cp.copy(lamda_ktild)
+        #print('lamda_k = ', lamda_k)
+        #print('lamda_k = ', lamda_ktild)
+
+    return TT, LL
+
 
 
 def permshape(T, perm, shp, order=None):
@@ -121,18 +177,27 @@ def permshape(T, perm, shp, order=None):
 
     if order == None:
         T = np.transpose(T, perm)
-        old_shape = T.shape
+        old_shape = list(T.shape)
         T = np.reshape(T, shp)
         reverse_perm = np.argsort(perm)
         return T, reverse_perm, old_shape
     if order == 'reverse':
         T = np.reshape(T, shp)
-        old_shape = T.shape
+        old_shape = list(T.shape)
         T = np.transpose(T, perm)
         reverse_perm = np.argsort(perm)
         return T, reverse_perm, old_shape
 
-def imaginary_time_evolution(right, left, eigen_k, unitary):
-    theta = None
+def imaginary_time_evolution(R, L, eigen_k, unitary):
+    eig_k_mat = np.zeros((len(eigen_k), len(eigen_k)), dtype=float)
+    np.fill_diagonal(eig_k_mat, eigen_k)
+    tensor_list = [R, L, eig_k_mat, unitary]
+    indices = ([-1, 1, 2], [-4, 3, 4], [2, 4], [1, -2, 3, -3])
+    order = [1, 2, 3, 4]
+    theta = tnc.scon(tensor_list, indices, order)
     return theta
+
+def print_shape(v, v_name):
+    name = v_name + '.shape = '
+    print(name, v.shape)
 
--- temp.py
+++ temp.py
@@ -1,11 +1,13 @@
 import numpy as np
 import copy as cp
 import simple_update_algorithm as su
+from scipy import linalg
 from random import shuffle
+import matplotlib.pyplot as plt
 
 d = 2
 p = 3
-D_max = d ** 2
+D_max = 2
 
 T0 = np.random.rand(p, d, d, d, d)
 T1 = np.random.rand(p, d, d, d, d)
@@ -26,11 +28,32 @@ smat = np.array([[1, 2, 3, 0, 4, 0, 0, 0],
 
 LL = []
 for i in range(8):
-    LL.append(np.ones((d), dtype=float) * i / 10)
+    LL.append(np.ones(d, dtype=float) / d)
 
+pauli_z = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1.]])
+pauli_y = np.array([[0, -1j, 0.], [1j, 0, -1j], [0, 1j, 0.]]) / np.sqrt(2)
+pauli_x = np.array([[0, 1, 0], [1, 0, 1], [0, 1., 0]]) / np.sqrt(2)
+
+t_list = np.linspace(0, 0.1, 100)
 Uij = np.random.rand(d, d)
+H_term = np.kron(pauli_x, pauli_x) + np.kron(pauli_y, pauli_y) + np.kron(pauli_z, pauli_z)
+hij = H_term.reshape(p, p, p, p)
+hij, _, _ = su.permshape(hij, [0, 2, 1, 3], [p ** 2, p ** 2])
+unitary = [np.reshape(linalg.expm(-t * hij), [p, p, p, p]) for t in range(len(t_list))]
+
+'''
+# check unitary values
+usum = []
+for j in range(len(unitary)):
+    usum.append(np.sum(np.real(unitary[j])))
+plt.figure()
+plt.plot(range(len(usum[0:80])), usum[0:80])
+plt.show()
+'''
 
 
-TT, LL = su.simple_update(TT, LL, Uij, imat, smat, D_max)
+for i in range(len(t_list)):
+    print('i = ', i)
+    TT, LL = su.simple_update(TT, LL, unitary[i], imat, smat, D_max)
 
 
